10583c98111aaf703d473cf1eba3aef5215407c7
Sharun <sharunksplus@gmail.com>
2023-07-17 15:40:00 -0400

    removed unused state

--- src/pages/inbox/MarkAllAsReadButton.tsx
+++ src/pages/inbox/MarkAllAsReadButton.tsx
@@ -1,12 +1,10 @@
-import { IonActionSheet, IonButton, IonIcon } from "@ionic/react";
+import { IonButton, IonIcon } from "@ionic/react";
 import { useAppDispatch } from "../../store";
 import { checkmarkDone } from "ionicons/icons";
-import { useState } from "react";
 import { markAllRead } from "../../features/inbox/inboxSlice";
 
 export default function MarkAllAsReadButton() {
   const dispatch = useAppDispatch();
-  const [open, setOpen] = useState(false);
 
   return (
     <>
