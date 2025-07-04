8dbea7a24c3836dfca31710c3dac14e2e3c1e44e
Sharun <sharun@sharunkumar.com>
2025-07-03 23:26:47 -0700

    fix: update comments and posts sorting structure for consistency

--- src/features/settings/custom.ts
+++ src/features/settings/custom.ts
@@ -5,11 +5,19 @@ export function custom(s: SettingsState): SettingsState {
   s.appearance.general.profileLabel = "handle";
   s.appearance.theme = "tangerine";
   s.appearance.voting.voteDisplayMode = "separate";
-  s.general.comments.sort = "Top";
+  s.general.comments.sort = {
+    lemmyv0: "Top",
+    lemmyv1: "Top",
+    piefed: "Top",
+  }
   s.general.enableHapticFeedback = false;
   s.general.posts.autoHideRead = true;
   s.general.posts.showHideReadButton = true;
-  s.general.posts.sort = "TopDay";
+  s.general.posts.sort = {
+    lemmyv0: "TopDay",
+    lemmyv1: "TopDay",
+    piefed: "TopDay",
+  };
   s.general.posts.upvoteOnSave = true;
   s.general.posts.markReadOnScroll = true;
   return s;
